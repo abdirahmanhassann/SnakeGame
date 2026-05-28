c48058b1e65ffc0cc9579c2ba140ad9d2a468c74
abdirahmanhassann <abdi12340.am@gmail.com>
2026-05-28 16:57:12 +0100

    Added initial snake and mouse values

--- SnakeGame/Form1.cs
+++ SnakeGame/Form1.cs
@@ -1,4 +1,7 @@
+using Microsoft.VisualBasic.Devices;
 using Microsoft.VisualBasic.Logging;
+using System.ComponentModel;
+using System.Drawing;
 using System.Timers;
 using System.Windows.Forms;
 
@@ -6,13 +9,27 @@ namespace SnakeGame
 {
     public partial class Form1 : Form
     {
-        public enum Directions { north = Keys.Up, east = Keys.Right, south = Keys.Down, west = Keys.Left }
+        public enum Directions
+        {
+            north = Keys.Up,
+            east = Keys.Right,
+            south = Keys.Down,
+            west = Keys.Left
+        }
         public Directions _snakeDirection = Directions.north;
-        static System.Windows.Forms.Timer _myTimer = new System.Windows.Forms.Timer();
-        //public int _
+        private System.Windows.Forms.Timer _myTimer = new System.Windows.Forms.Timer();
+        public List<PictureBox> _Snake = new();
+        public int _x;
+        public int _y;
+        public int gridSize = 15;
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            InitialValues();
             _myTimer.Tick += new EventHandler(GameLoop);
             _myTimer.Interval = 200;
             _myTimer.Start();
@@ -43,10 +60,67 @@ namespace SnakeGame
         private void InitialValues()
         {
             _snakeDirection = Directions.west;
+            _x = this.ClientSize.Width / gridSize;
+            _y = this.ClientSize.Height / gridSize;
+            _Snake.Add(CreateSnakePictureBox());
+            GenerateMousePosition();
+        }
+        private PictureBox CreateSnakePictureBox()
+        {
+            Random ran = new();
+            PictureBox snakePictureBox = new();
+            snakePictureBox.Name = $"snakePictureBox{_Snake.Count + 1}";
+            snakePictureBox.Location = new Point(GenerateInitialSnakePosition(), GenerateInitialSnakePosition());
+            snakePictureBox.Size = new Size(this.ClientSize.Width / gridSize, this.ClientSize.Height / gridSize);
+            snakePictureBox.TabIndex = 0;
+            snakePictureBox.TabStop = false;
+            snakePictureBox.BackColor = Color.FromArgb(255, 255, 20);
+            this.Controls.Add(snakePictureBox);
+            return snakePictureBox;
+        }
+        private int GenerateInitialSnakePosition()
+        {
+            Random rand = new();
+            int axis = rand.Next(1, gridSize);
+            return axis * _x;
+        }
+
+        private void GenerateMousePosition()
+        {
+            bool isBallOnSnake;
+            Random rand = new();
+            while (true)
+            {
+                int[] mousePosition = new int[2];
+                mousePosition[0] = rand.Next(0, this.ClientSize.Width);
+                mousePosition[1] = rand.Next(0, this.ClientSize.Width);
+                //formula to generate ball on square grid.
+                //wrote this a couple months ago
+                //dont remember why i took this approach.
+                //not the most efficient code, but it works and i cba figuring out a better way.
+                mousePosition[0] = mousePosition[0] - (mousePosition[0] % gridSize);
+                mousePosition[1] = mousePosition[1] - (mousePosition[1] % gridSize);
+
+                isBallOnSnake = false;
+                for (int i = 0; i < _Snake.Count; i++)
+                {
+                    if (_Snake[i].Location.X == mousePosition[0] && _Snake[i].Location.Y == mousePosition[1])
+                    {
+                        isBallOnSnake = true;
+                        break;
+                    }
+                }
+                if (!isBallOnSnake)
+                {
+                    mousePictureBox.Location = new Point(mousePosition[0], mousePosition[1]);
+                    break;
+                    // return _ballPosition;
+                }
+            }
 
         }
 
-        private void Form1_Load_1(object sender, EventArgs e)
+        private void mousePictureBox_Click(object sender, EventArgs e)
         {
 
         }
